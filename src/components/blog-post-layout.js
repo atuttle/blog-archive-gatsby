1247084d64f76e2bbb3384bc6bcf3bab1bb58a4a
Adam Tuttle <adamtuttlecodes@gmail.com>
2019-05-01 14:28:20 -0400

    fix twitter embeds yay

--- src/components/blog-post-layout.js
+++ src/components/blog-post-layout.js
@@ -13,13 +13,26 @@ const BlogPostLayout = ({ children, pageContext }) => {
 			</Helmet>
 			<article>
 				<header style={{ marginBottom: `20px` }}>
-					<h1><Link to="/blog">Blog</Link> / {title}</h1>
-					<time style={{
-						display: `inline-block`,
-						padding: `6px 10px`,
-						fontSize: `0.8em`
-					}}>
-						<span style={{ fontSize: `2.3em`, display: `inline-block`, position: `relative`, top: `0.15em` }}>👨‍💻</span>
+					<h1>
+						<Link to="/blog">Blog</Link> / {title}
+					</h1>
+					<time
+						style={{
+							display: `inline-block`,
+							padding: `6px 10px`,
+							fontSize: `0.8em`
+						}}
+					>
+						<span
+							style={{
+								fontSize: `2.3em`,
+								display: `inline-block`,
+								position: `relative`,
+								top: `0.15em`
+							}}
+						>
+							👨‍💻
+						</span>
 						{` `}By Adam Tuttle on {format(date, `MMM DD, YYYY`)}
 					</time>
 				</header>
