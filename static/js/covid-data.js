8af1d79b9a188bf881f4b839fc7294a6dd4be1f8
Adam Tuttle <adamtuttlecodes@gmail.com>
2020-04-28 10:28:06 -0400

    tryin'a fix this covid data thing

---
+++ static/js/covid-data.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+	var dataPoints = [];
+
+	var chart = new CanvasJS.Chart('chartContainer', {
+		animationEnabled: true,
+		theme: 'dark2',
+		title: {
+			text: 'COVID19 Daily Infection Delta (Chester County, PA)'
+		},
+		axisY: {
+			title: 'New Confirmed Infections',
+			titleFontSize: 24
+		},
+		data: [
+			{
+				type: 'column',
+				yValueFormatString: '#,### new confirmed infections',
+				dataPoints: dataPoints
+			}
+		]
+	});
+
+	function addData(data) {
+		console.log(data);
+		for (var i = 0; i < data.features.length; i++) {
+			dataPoints.push({
+				x: new Date(data.features[i].attributes.Date),
+				y: data.features[i].attributes.Positives_Daily
+			});
+		}
+		chart.render();
+	}
+
+	$.getJSON(
+		'https://services.arcgis.com/G4S1dGvn7PIgYd6Y/arcgis/rest/services/COVID_19_Cases_Chester_County__PA/FeatureServer/2/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc&outSR=102100&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true',
+		addData
+	);
+};
