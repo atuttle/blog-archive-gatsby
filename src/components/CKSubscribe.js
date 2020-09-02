3b8eb49585c1334df45c8eb0885484d095370c05
Adam Tuttle <adamtuttlecodes@gmail.com>
2020-09-01 23:48:03 -0400

    archive of an email I sent in august / trying out convertkit

---
+++ src/components/CKSubscribe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CKSubscribe = function() {
+	return (
+		<script
+			async
+			data-uid="02c5dc9bec"
+			src="https://adam-tuttle.ck.page/02c5dc9bec/index.js"
+		></script>
+	);
+};
+
+export default CKSubscribe;
