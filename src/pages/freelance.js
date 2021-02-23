d2c505d58a965cbb7abb88302103a89f04594810
Adam Tuttle <adamtuttlecodes@gmail.com>
2021-02-23 18:01:31 -0500

    added freelance page

---
+++ src/pages/freelance.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import FluidYouTube from '../components/FluidYouTube';
+
+const FreelancePage = () => (
+	<Layout>
+		<SEO title="Freelance Web Development by Adam Tuttle" />
+		<h1>I am available for freelance web development gigs</h1>
+		<p>
+			Here because of <a href="https://taffy.io">Taffy</a>? If you're not aware,
+			Taffy is an open source framework I created and maintain for authoring
+			REST API's in CFML.
+		</p>
+		<p>
+			Most of the freelance work that comes my way is people having trouble with
+			their Taffy API who want to pay me to figure out the problem and fix it
+			for them. This type of job is one that I am happy to consider. You should
+			also know that there is a community of people who are usually very willing
+			to help out with small problems (for free!) in the <strong>#taffy</strong>{' '}
+			channel on the <a href="https://cfml-slack.herokuapp.com/">CFML Slack</a>.
+			Sometimes I do, too.
+		</p>
+		<p>
+			I also occasionally take on small projects that are entirely independent
+			of Taffy. I am an expert CFML/ColdFusion developer, and a pretty decent
+			Node.js/React developer, and those are the opportunities that will most
+			interest me. If you've got a project you think will interest me, read on.
+		</p>
+		<p>
+			I am happy employed at <a href="https://www.alumniq.com/">AlumnIQ</a>{' '}
+			which both means that I am not seeking a new employer, and that I am not
+			available at all during normal business hours (at least not in American
+			time zones). All of my freelance work is done on a strictly nights &amp;
+			weekends schedule, with availability in the 5-20 hours per week range
+			depending on what else is going on (very seasonal).
+		</p>
+		<p>
+			I have no interest in working for stock options, profit sharing, or
+			"exposure."
+		</p>
+		<p>
+			If all of the above sounds good to you, here's the last thing you need to
+			know: My rate is a minimum of USD $200 per hour. I am also willing to
+			consider fixed-price projects if they're interesting and/or lucrative
+			enough.
+		</p>
+		<p>
+			Email your freelancing requests to me at{' '}
+			<a href="mailto:adamtuttlecodes@gmail.com">adamtuttlecodes@gmail.com</a>.
+			If you've ignored the information above, I probably won't reply.
+		</p>
+	</Layout>
+);
+
+export default FreelancePage;
