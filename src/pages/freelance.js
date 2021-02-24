879e14fedd0f4ac25ab4c968f3a9d98385c68966
Adam Tuttle <adamtuttlecodes@gmail.com>
2021-02-24 06:54:33 -0500

    freelance page updates

--- src/pages/freelance.js
+++ src/pages/freelance.js
@@ -8,18 +8,17 @@ const FreelancePage = () => (
 		<SEO title="Freelance Web Development by Adam Tuttle" />
 		<h1>I am available for freelance web development</h1>
 		<p>
-			Here because of <a href="https://taffy.io">Taffy</a>? If you're not aware,
-			Taffy is an open source framework I created and maintain for authoring
-			REST API's in CFML.
+			Here because of <a href="https://taffy.io">Taffy</a>? Taffy is an open
+			source framework I created for authoring REST API's in CFML.
 		</p>
 		<p>
 			Most of the freelance work that comes my way is people having trouble with
 			their Taffy API who want to pay me to figure out the problem and fix it
-			for them. This type of job is one that I am happy to consider. You should
-			also know that there is a community of people who are usually very willing
-			to help out with small problems (for free!) in the <strong>#taffy</strong>{' '}
-			channel on the <a href="https://cfml-slack.herokuapp.com/">CFML Slack</a>.
-			Sometimes I do, too.
+			for them. This type of job is one that I am happy to consider. There is
+			also a community of people who are usually very willing to help out with
+			small problems (for free!) in the <strong>#taffy</strong> channel on the{' '}
+			<a href="https://cfml-slack.herokuapp.com/">CFML Slack</a>. Sometimes I
+			do, too.
 		</p>
 		<p>
 			I also occasionally take on small projects that are entirely independent
@@ -30,10 +29,10 @@ const FreelancePage = () => (
 		<p>
 			I am happily employed at <a href="https://www.alumniq.com/">AlumnIQ</a>{' '}
 			which both means that I am not seeking a new employer, and that I am not
-			available at all during normal business hours (at least not in American
-			time zones). All of my freelance work is done on a strictly nights &amp;
-			weekends schedule, with availability in the 5-20 hours per week range
-			depending on what else is going on (very seasonal).
+			available at all from 8am&ndash;6pm in the US/Eastern time zone. All of my
+			freelance work is done on a strictly nights &amp; weekends schedule, with
+			availability in the 5-20 hours per week range depending on what else is
+			going on (very seasonal).
 		</p>
 		<p>
 			I have no interest in working for stock options, profit sharing, or
@@ -41,9 +40,8 @@ const FreelancePage = () => (
 		</p>
 		<p>
 			If all of the above sounds good to you, here's the last thing you need to
-			know: My rate is a minimum of USD $200 per hour. I am also willing to
-			consider fixed-price projects if they're interesting and/or lucrative
-			enough.
+			know: My usual rate is USD $200 per hour. I am also willing to consider
+			fixed-price projects if they're interesting and/or lucrative enough.
 		</p>
 		<p>
 			Email your freelancing requests to me at{' '}
